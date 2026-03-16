71bde785f9a7f767606f7cdf16da808c7528997b
lelisa21 <lelisahailu21@gmail.com>
2026-03-16 21:23:04 +0300

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl border border-gray-200 p-4 m-2.5'>Employee Managment System</div>
+  )
+}
+
+export default App
+
