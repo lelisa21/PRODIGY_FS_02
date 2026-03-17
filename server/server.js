42c15be72fad16a56c017c564fa08e8aa931ac2c
lelisa21 <lelisahailu21@gmail.com>
2026-03-17 21:52:31 +0300

    feat: all CRUD operations implemented on backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+import employeeRoutes from "./routes/emloyee.routes.js"
+
+
 const app = express();
 
 // middleware
@@ -7,6 +10,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/auth', authRoutes);
+app.use("/api/employees", employeeRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
