d07d16c10d66c57b878b649593fd6054fc30bb07
lelisa21 <lelisahailu21@gmail.com>
2026-03-17 22:07:35 +0300

    doc: just for what i have to do next on backend production ready API

--- server/server.js
+++ server/server.js
@@ -12,6 +12,12 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use("/api/employees", employeeRoutes);
 
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message || "Server Error"
+  })
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
