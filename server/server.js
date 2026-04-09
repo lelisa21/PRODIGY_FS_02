c46318c6ef7062827b927d1f2e321e5ebe3fbdfd
lelisa21 <lelisahailu21@gmail.com>
2026-04-09 14:19:19 +0300

    feat: redis fixed now added base url to see general message

--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'active',
+    message: 'Employee Management Server is running',
+    environment: process.env.NODE_ENV
+  });
+});
+
 // Handle uncaught exceptions
 process.on("uncaughtException", (error) => {
   logger.error("Uncaught Exception:", error);
@@ -37,6 +45,7 @@ const startServer = async () => {
 
     const server = http.createServer(app);
     initSocket(server);
+
 
     server.listen(PORT, () => {
       logger.info(
